assorted fixes, normalization + summarywriter

src/mbrl/agents.py
@@ -48,30 +48,33 @@ class Agent:
             for i in range(self.num_rollouts_per_iteration)
         ]
         self.dataset.add_rollouts(rollouts)
-        logger.record_tabular('AvgSumRewardPerRollout', np.mean([sum(filter(bool, rollout.rewards)) for rollout in rollouts]))
-        logger.record_tabular('AvgSumCostPerRollout', np.mean([sum(map(self.state_cost, rollout.states)) for rollout in rollouts]))
+        # logger.record_tabular('AvgSumRewardPerRollout', np.mean([sum(filter(bool, rollout.rewards)) for rollout in rollouts]))
+        # logger.record_tabular('AvgSumCostPerRollout', np.mean([sum(map(self.state_cost, rollout.states)) for rollout in rollouts]))
 
     def train(self):
         logger.info('Starting outer training loop.')
-        logger.record_tabular('Itr', 0)
-        self.add_rollouts()
+        # logger.record_tabular('Itr', 0)
+        for _ in range(10):
+            self.add_rollouts()
         logger.dump_tabular()
         for iteration in range(1, self.num_train_iterations + 1):
-            logger.record_tabular('Itr', iteration)
+            # logger.record_tabular('Itr', iteration)
             logger.debug('Iteration {}'.format(iteration))
             logger.debug('Training model with batch size {} for {} epochs'.format(self.batch_size, self.num_epochs_per_iteration))
             self.model.train_model(dataset=self.dataset, optimizer=self.optimizer, batch_size=self.batch_size, num_epochs=self.num_epochs_per_iteration)
             self.add_rollouts(get_action=self.get_action)
-            logger.dump_tabular()
-
-
+            # logger.dump_tabular()
+def compose(f, g):
+    def wrap(*args, **kwargs):
+        return g(f(*args, **kwargs))
+    return wrap
 class MPCAgent(Agent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.last_trajectory = None
 
     def get_action(self, state: torch.Tensor) -> torch.Tensor:
-        logger.debug('Planning a step. Horizon:{}'.format(self.horizon))
+        # logger.debug('Planning a step. Horizon:{}'.format(self.horizon))
         if self.last_trajectory is not None:
             initial_trajectory = (
                 self.last_trajectory[0][1:],
@@ -80,11 +83,11 @@ class MPCAgent(Agent):
         else:
             initial_trajectory = None
         self.last_trajectory = self.planner.plan(
-            initial_state=state,
+            initial_state=self.dataset.normalize_state(state),
             model=partial(self.model, unnormalize=self.dataset.unnormalize_state),
             state_cost=self.state_cost,
             action_cost=self.action_cost,
-            sample_action=self.environment.sample_action,
+            sample_action=compose(self.environment.sample_action, self.dataset.normalize_action),
             horizon=self.horizon,
             initial_trajectory=initial_trajectory,
         )

src/mbrl/models.py
@@ -5,6 +5,10 @@ from src.mbrl.data import TransitionsDataset, TransitionsSampler
 from src.mbrl.logger import logger
 
 class DynamicsModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.train_iterations = 0
+        self.writer = None
     def forward(self, x, unnormalize=None):
         out = self._forward(x)
         if unnormalize:
@@ -29,9 +33,8 @@ class DynamicsModel(nn.Module):
         if not criterion:
             criterion = torch.nn.MSELoss()
         train_data = DataLoader(dataset, batch_size=batch_size, sampler=TransitionsSampler(dataset))
+        num_iters = 0
         for epoch in range(num_epochs):
-            epoch_loss = 0
-            num_iters = 0
             for inputs, outputs in train_data:
                 loss = 0
                 for ((states, observations, actions), (rewards, next_states, next_observations)) in zip(inputs, outputs):
@@ -42,20 +45,11 @@ class DynamicsModel(nn.Module):
                 optimizer.zero_grad()
                 loss.backward(retain_graph=True)
                 optimizer.step()
-                epoch_loss += loss
                 num_iters += 1
 
-            epoch_loss = float(epoch_loss.detach().numpy())
-            if epoch == 0:
-                logger.record_tabular("LossFirstEpoch", epoch_loss/num_iters)
-                logger.record_tabular("NumBatchesPerEpoch", num_iters)
-        logger.record_tabular("LossLastEpoch", epoch_loss/num_iters)
-
-            # if self.writer is not None:
-            #     self.writer.add_scalar('loss/state/{}'.format(iteration), loss, epoch)
-            #     self.writer.add_scalar('single_step_loss/{}'.format(iteration), single_loss, epoch)
-
-
+                if self.writer is not None:
+                    self.writer.add_scalar('loss/state/{}'.format(self.train_iterations), loss, num_iters)
+        self.train_iterations += 1
 class Model(DynamicsModel):
     def __init__(self, state_dim, action_dim, hidden_units=50, noise=None):
         super(Model, self).__init__()

src/mbrl/test_agents.py
@@ -9,8 +9,10 @@ environment = EnvWrapper.load('reacher', 'easy', visualize_reward=True)
 from src.mbrl.planners import RandomShootingPlanner
 planner = RandomShootingPlanner
 from src.mbrl.models import Model, CoshLoss, SmoothAbsLoss
-model = Model(environment.state_dim, environment.action_dim)
+from tensorboardX import SummaryWriter
 
+model = Model(environment.state_dim, environment.action_dim)
+model.writer = SummaryWriter(log_dir=exp_dir)
 l2_penalty = 0  # 0.001
 learn_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate, weight_decay=l2_penalty)

src/mbrl/test_train_model.py
@@ -5,7 +5,10 @@ from src.mbrl.data import TransitionsDataset
 dataset = TransitionsDataset(rollouts=[], transitions_capacity=100000)
 
 from src.mbrl.models import Model
+from tensorboardX import SummaryWriter
+
 model = Model(environment.state_dim, environment.action_dim)
+model.writer = SummaryWriter(log_dir='model-test')
 
 from torch.optim import SGD
 optimizer = SGD(model.parameters(), lr=0.1)
@@ -19,11 +22,11 @@ rollouts = [
 dataset.add_rollouts(rollouts)
 
 import numpy as np
-evals = model.evaluate_model(dataset=dataset, batch_size=50)
+evals = model.evaluate_model(dataset=dataset, batch_size=500)
 before_train_loss = np.mean(evals)
 print('Before train loss:', before_train_loss)
 
-model.train_model(dataset=dataset, optimizer=optimizer, batch_size=50, num_epochs=2)
+model.train_model(dataset=dataset, optimizer=optimizer, batch_size=500, num_epochs=2)
 
 evals = model.evaluate_model(dataset=dataset, batch_size=50)
 after_train_loss = np.mean(evals)